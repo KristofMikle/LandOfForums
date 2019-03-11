e3b132c555c5e35e0ae037beb0721d1bbb1978ce
KristofMikle <bitangjooo@gmail.com>
2019-03-11 09:59:34 +0100

    Initial commit, created Data Project

---
+++ LandOfForums.Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using LandOfForums.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LandOfForums.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<Forum> Forums { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<PostReply> PostsReplies { get; set; }
+    }
+}
---
+++ LandOfForums.Data/Models/Forum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LandOfForums.Data.Models
+{
+    public class Forum
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImageURL { get; set; }
+        public DateTime Created { get; set; }
+
+        public virtual IEnumerable<Post> Posts { get; set; }
+    }
+}
